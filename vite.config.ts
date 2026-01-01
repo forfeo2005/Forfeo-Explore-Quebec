5dc6f52cd3faae17e701d43567300e1386b0a9f6
forfeo2005 <forfeo2005@gmail.com>
2026-01-01 17:43:09 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/forfeo-explore-quebec/', // <-- LIGNE COMMENTÉE POUR LE DEV LOCAL
+  base: '/forfeo-explore-quebec/', // <--- LIGNE ACTIVÉE (sans les // au début)
 })
