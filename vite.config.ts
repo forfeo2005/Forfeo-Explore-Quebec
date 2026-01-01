fbffc8628315d3ecf99ea37e0d0b458407509f1a
forfeo2005 <forfeo2005@gmail.com>
2026-01-01 18:22:12 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/forfeo-explore-quebec/', // <--- LIGNE ACTIVÉE (sans les // au début)
+  base: '/Forfeo-Explore-Quebec/', // <--- Majuscules F, E, Q obligatoires pour GitHub !
 })
