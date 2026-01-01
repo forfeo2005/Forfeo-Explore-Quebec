90fdb2a10040f1b6ce2e2382dee0d82b93acdc93
forfeo2005 <forfeo2005@gmail.com>
2026-01-01 16:32:43 -0500

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/forfeo-explore-quebec/', // Requis pour le déploiement GitHub Pages
+})
