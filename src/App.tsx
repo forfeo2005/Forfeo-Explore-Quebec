56d99903cd3d2cd0e3a3162050f024d5f72d41fd
forfeo2005 <forfeo2005@gmail.com>
2026-01-02 01:22:52 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 import RegionDetail from './pages/RegionDetail';
-import Blog from './Blog'; 
-
-// J'ai retiré <Header /> d'ici pour ne pas l'avoir en double sur l'accueil.
-// Nous allons l'ajouter manuellement dans la page Blog à l'étape suivante.
+import Blog from './Blog';
+import Legal from './pages/Legal'; // Import de la nouvelle page
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <Header />
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/regions" element={<Blog />} />
           <Route path="/region/:id" element={<RegionDetail />} />
           <Route path="/blog/:id" element={<RegionDetail />} />
+          <Route path="/legal" element={<Legal />} /> {/* Nouvelle route */}
         </Routes>
       </main>
     </div>
