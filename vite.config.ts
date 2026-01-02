6ea047f397c5225bcf75edf89490697052977915
forfeo2005 <forfeo2005@gmail.com>
2026-01-01 19:06:58 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Nous utilisons le chemin ABSOLU exact de votre dépôt GitHub
-  // Cela empêche les erreurs de navigation
-  base: '/Forfeo-Explore-Quebec/', 
+  base: '', // <-- LAISSEZ VIDE (Juste deux guillemets simples)
 })
