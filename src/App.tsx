e98c60e8133c861bc4c77619d79050ec58f1ff80
forfeo2005 <forfeo2005@gmail.com>
2026-01-01 23:05:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import RegionDetail from './pages/RegionDetail'; // <--- Importation de la nouvelle page
+import RegionDetail from './pages/RegionDetail'; // Importation de la nouvelle page
 
 function App() {
   return (
     <Routes>
-      {/* Route Accueil */}
       <Route path="/" element={<Home />} />
-      
-      {/* Route Dynamique : "id" changera selon la région cliquée */}
+      {/* C'est cette ligne qui active les nouvelles pages ! */}
       <Route path="/region/:id" element={<RegionDetail />} />
     </Routes>
   );
